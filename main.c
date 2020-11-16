7e02e02ac759050b4fdcf1fcf1e0c548fa7e68cf
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2020-11-16 18:27:22 +0000

    Added createGrid() function
    Created the createGrid() function to return a pointer to memory allocated for a Grid structure

--- main.c
+++ main.c
@@ -3,12 +3,39 @@
 
 #define MAXSIZE 20
 
-
 typedef struct grid{
     char grid[MAXSIZE][MAXSIZE];
     int rows;
     int cols;
 }Grid;
+
+Grid* createGrid(){
+    Grid* pGrid = NULL;    
+    pGrid = (Grid*)malloc(sizeof(Grid));
+    if (pGrid==NULL)
+        return NULL;
+
+
+    int valid = 0;
+    while(valid == 0){
+        printf("Grid Size (ROW COL): ");
+        scanf("%d %d", &(pGrid->rows), &(pGrid->cols));
+
+        if(pGrid->rows<=MAXSIZE && pGrid->cols<=MAXSIZE){
+            valid = 1;
+        }else{
+            printf("Please enter a gride size under 20x20");
+        }
+    }
+
+    for(int i=0; i<pGrid->rows; i++){
+        for (int j=0; j<pGrid->cols; j++){
+            pGrid->grid[i][j] = 'O';
+        }
+    }
+
+    return pGrid;
+}
 
 int main() {
 
